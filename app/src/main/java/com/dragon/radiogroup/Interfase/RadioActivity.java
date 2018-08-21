54b77aa667b41a916c259a340cb76c8abaa3fa77
MakusuMax <40814622+vloxomire@users.noreply.github.com>
2018-08-21 13:43:27 -0300

    terminado
    Falta los intent

--- app/src/main/java/com/dragon/radiogroup/Interfase/RadioActivity.java
+++ app/src/main/java/com/dragon/radiogroup/Interfase/RadioActivity.java
@@ -3,6 +3,7 @@ package com.dragon.radiogroup.Interfase;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -14,18 +15,32 @@ public class RadioActivity extends Activity {
     private EditText et1, et2;
     private RadioButton rb1, rb2;
     private TextView tv1;
-    private Button b1;
+    private Button b1, b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.radio);
-
-        et1=findViewById(R.id.et1);
-        et2=findViewById(R.id.et2);
-        rb1=findViewById(R.id.rb1);
-        rb2=findViewById(R.id.rb2);
-        tv1=findViewById(R.id.tv1);
-        b1=findViewById(R.id.b1);
+        //Metodo findViewById vinculo objetos con el atributo id
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
+        rb1 = findViewById(R.id.rb1);
+        rb2 = findViewById(R.id.rb2);
+        tv1 = findViewById(R.id.tv1);
+        b1 = findViewById(R.id.b1);
+        b2 = findViewById(R.id.b2);
+    }
+    public void calcular(View v){
+        int valor1 = Integer.parseInt(et1.getText().toString());
+        int valor2 = Integer.parseInt(et2.getText().toString());
+        if (rb1.isChecked() == true) {
+            int suma = valor1 + valor2;
+            String resu = String.valueOf(suma);
+            tv1.setText(resu);
+        } else if (rb2.isChecked() == true) {
+            int resta = valor1 - valor2;
+            String resu = String.valueOf(resta);
+            tv1.setText(resu);
+        }
     }
 }
